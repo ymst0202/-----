fee87abef98444153da8ef0ae477113a5ca29b04
山下陽希 <yamashita@w-crwe.co.jp>
2026-03-30 17:00:50 +0900

    20260330_クリップ選択→ターゲットオンに条件変更

--- Premier/トラックロックセレクター.jsx
+++ Premier/トラックロックセレクター.jsx
@@ -17,47 +17,41 @@
         var videoTrackCount = seq.videoTracks.numTracks;
         var audioTrackCount = seq.audioTracks.numTracks;
 
-        var selectedVideoTrackIndices = [];
-        var selectedAudioTrackIndices = [];
-        var hasAnySelection = false;
+        var targetedVideoTrackIndices = [];
+        var targetedAudioTrackIndices = [];
+        var hasAnyTarget = false;
 
-        // 1. ビデオトラックを走査して選択クリップを特定
+        // 1. ビデオトラックを走査してターゲットがオンのトラックを特定
         for (var i = 0; i < videoTrackCount; i++) {
             var track = seq.videoTracks[i];
-            for (var j = 0; j < track.clips.numItems; j++) {
-                if (track.clips[j].isSelected()) {
-                    selectedVideoTrackIndices.push(i);
-                    hasAnySelection = true;
-                    break;
-                }
+            // トラックのターゲット状態を確認
+            if (track.isTargeted && track.isTargeted()) {
+                targetedVideoTrackIndices.push(i);
+                hasAnyTarget = true;
             }
         }
 
-        // 2. オーディオトラックを走査して選択クリップを特定
+        // 2. オーディオトラックを走査してターゲットがオンのトラックを特定
         for (var i = 0; i < audioTrackCount; i++) {
             var track = seq.audioTracks[i];
-            for (var j = 0; j < track.clips.numItems; j++) {
-                if (track.clips[j].isSelected()) {
-                    selectedAudioTrackIndices.push(i);
-                    hasAnySelection = true;
-                    break;
-                }
+            // トラックのターゲット状態を確認
+            if (track.isTargeted && track.isTargeted()) {
+                targetedAudioTrackIndices.push(i);
+                hasAnyTarget = true;
             }
         }
 
-        // 3. 選択チェック
-        if (!hasAnySelection) {
-            alert("クリップが一つも選択されていません。\nロック解除しておきたいトラック上のクリップを選択してから実行してください。");
+        // 3. ターゲットチェック（一つもターゲットがオンになっていない場合）
+        if (!hasAnyTarget) {
+            alert("ターゲットがオンになっているトラックがありません。\nロック解除しておきたいトラックのターゲット（V1, A1など）をオンにしてから実行してください。");
             return;
         }
 
         // 4. トラックのロック状態を更新 (ビデオ)
         for (var i = 0; i < videoTrackCount; i++) {
             var track = seq.videoTracks[i];
-            // 選択クリップが含まれない場合はロック
-            var shouldLock = !contains(selectedVideoTrackIndices, i);
-            
-            // 【修正ポイント】 true/false を 1(ロック) / 0(解除) の数値に変換
+            // ターゲットが含まれない場合はロック
+            var shouldLock = !contains(targetedVideoTrackIndices, i);
             var lockParam = shouldLock ? 1 : 0; 
             track.setLocked(lockParam);
         }
@@ -65,9 +59,8 @@
         // 5. トラックのロック状態を更新 (オーディオ)
         for (var i = 0; i < audioTrackCount; i++) {
             var track = seq.audioTracks[i];
-            var shouldLock = !contains(selectedAudioTrackIndices, i);
-            
-            // 【修正ポイント】 true/false を 1/0 に変換
+            // ターゲットが含まれない場合はロック
+            var shouldLock = !contains(targetedAudioTrackIndices, i);
             var lockParam = shouldLock ? 1 : 0; 
             track.setLocked(lockParam);
         }
